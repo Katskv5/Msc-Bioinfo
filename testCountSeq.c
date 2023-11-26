b8ef70227ba0d1fa44a88ecadfcc01bf4a6d8e82
Katskv5 <145585686+Katskv5@users.noreply.github.com>
2023-11-26 18:20:49 +0200

    Create testCountSeq.c
    This command line calculates the Number of sequences inserted by the user.

---
+++ testCountSeq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+int main()
+
+
+{
+      int length=0;
+      char seq[10000];
+      int i=0;
+      int count;
+
+     while ( scanf ("%s", seq) == 1 )
+     {
+       for (i=0; i<10000; i++);
+       count++;
+       printf(" The number of sequence(s) is %d\n", count);
+     }
+
+}
